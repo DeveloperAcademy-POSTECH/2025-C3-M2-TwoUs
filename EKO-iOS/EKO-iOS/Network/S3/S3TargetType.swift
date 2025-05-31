ef3000f3dddc523d0745f08ba48322314eafada6
dangdang1ing15 <shine_0512@naver.com>
2025-05-31 18:33:24 +0900

    [Feat] #17 - S3 다운로드 링크 생성 네트워크 코드 구현

--- EKO-iOS/EKO-iOS/Network/S3/S3TargetType.swift
+++ EKO-iOS/EKO-iOS/Network/S3/S3TargetType.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import Moya
+
+enum S3TargetType{
+    case fetchS3DownloadURL(s3Key: String)
+}
+
+extension S3TargetType: BaseTargetType {
+    var utilPath: UtilPath { return .s3 }
+    var pathParameter: String? { return .none }
+    
+    var headerType: [String: String?]{
+        switch self {
+        case .fetchS3DownloadURL:
+            return ["Content-Type": "application/json"]
+        }
+    }
+    
+    var queryParameter: [String : Any]? {
+        switch self {
+        case .fetchS3DownloadURL(let s3Key):
+            return ["s3Key": s3Key]
+        }
+    }
+    
+    var requestBodyParameter: Codable? {
+        switch self {
+        case .fetchS3DownloadURL: return .none
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchS3DownloadURL: return utilPath.rawValue + "presign"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .fetchS3DownloadURL: return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .fetchS3DownloadURL(s3Key):
+            return .requestParameters(parameters: ["s3Key": s3Key],
+                                      encoding: JSONEncoding.default)
+        }
+    }
+}
--- EKO-iOS/FetchS3DownloadURLResposeDTO.swift
+++ EKO-iOS/FetchS3DownloadURLResposeDTO.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct FetchS3DownloadURLResposeDTO: Decodable {
+    let url: String
+}
