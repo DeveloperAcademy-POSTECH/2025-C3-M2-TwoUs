f8ac303f9a5c9213fd8f7d75fe83c811384d586a
dangdang1ing15 <shine_0512@naver.com>
2025-05-31 17:48:36 +0900

    [Feat] #17 - 피드백 전송 네트워크 코드 구현

---
+++ EKO-iOS/PostStartFeedbackResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  PostStartFeedbackResponseDTO.swift
+//  EKO-iOS
+//
+//  Created by 성현 on 5/31/25.
+//
+
+import Foundation
+
+struct PostStartFeedbackResponseDTO: Decodable {
+    let success: Bool
+    let sessionId: String
+}
